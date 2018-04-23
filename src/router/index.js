341d576a55f22f61f51adf11ea7f1a04ab02d64a
zhaiyingying-bd <zhaiyingying-bd@ofo.com>
2018-04-23 16:12:16 +0800

    add cart vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from '@/views/goodsList'
+import Cart from '@/views/cart'
 
 
 Vue.use(Router)
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'GoodsList',
       component: GoodsList
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
     },
   ]
 })
